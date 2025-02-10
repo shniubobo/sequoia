bdd9567cd94184150560b5dfb8826dd59779fa93
Mrmaxmeier <Mrmaxmeier@gmail.com>
2025-02-10 22:52:32 +0100

    fuzz: fix decrypt_from_bytes harness

--- openpgp/fuzz/fuzz_targets/decrypt_from_bytes.rs
+++ openpgp/fuzz/fuzz_targets/decrypt_from_bytes.rs
@@ -60,18 +60,15 @@ fuzz_target!(|data: &[u8]| -> Corpus {
         }
     }
     impl DecryptionHelper for Helper {
-        fn decrypt<D>(
+        fn decrypt(
             &mut self,
             _: &[PKESK],
             _: &[SKESK],
             _sym_algo: Option<SymmetricAlgorithm>,
-            mut decrypt: D,
-        ) -> Result<Option<openpgp::Fingerprint>>
-        where
-            D: FnMut(SymmetricAlgorithm, &SessionKey) -> bool,
-        {
-            decrypt(SymmetricAlgorithm::AES128, &SK);
-            Ok(Some(FP.clone()))
+            decrypt: &mut dyn FnMut(Option<SymmetricAlgorithm>, &SessionKey) -> bool,
+        ) -> Result<Option<openpgp::Cert>> {
+            decrypt(Some(SymmetricAlgorithm::AES128), &SK);
+            Ok(Some(CERT.clone()))
         }
     }
 
